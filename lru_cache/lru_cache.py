5d4e145cc82ab10dbd9bc9a391a16f0f7063425f
johanaluna <johanaluna@users.noreply.github.com>
2020-02-26 01:40:21 -0500

    Wednesday assingment finished

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +12,12 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.dll = DoublyLinkedList()
+        self.limit=limit
+        # dictiionary: to save the key,value
+        self.dict = dict()
+        self.size = 0
+        
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +27,20 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # If key-value pair exists in the cache
+        if key in self.dict:
+            # set the node to the item at the key in dictionary
+            # self.dict[key] has the value then node = value
+            node = self.dict[key]
+            # move the node to end of dll
+            self.dll.move_to_end(node)
+            # return the nodes value
+            # Position0:key, Position1:value
+            return node.value[1]
+        # otherwise: key-value pair doesn't exist in the cache
+        else:
+            # return None
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +53,26 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # if key exists in storage
+        if key in self.dict:
+            # set the node to the storage at the key
+            node = self.dict[key]
+            # set the nodes value to the key value pair
+            node.value = (key, value)
+            # move the node to the end and 
+            self.dll.move_to_end(node)
+            return
+        
+        # if cache is already at max capacity
+        if self.size == self.limit:
+            # Remove the oldest entry in the cache
+            del self.dict[self.dll.head.value[0]]
+            # Remove the node
+            self.dll.remove_from_head()
+            self.size -= 1
+            
+        # add a new node (key, value) to the tail (newest entry)
+        self.dll.add_to_tail((key, value))
+        # set the key in the dictionary
+        self.dict[key] = self.dll.tail
+        self.size += 1
